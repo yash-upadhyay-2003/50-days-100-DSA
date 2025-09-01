fb9c2d44c9c59fde3a021b6711a5122d56714b95
yash-upadhyay-2003 <yash.upadhyay2022@glbajajgroup.org>
2025-09-01 19:00:07 +0530

    added SecondLargestInArray.java

---
+++ SercondLargestInArray.java
@@ -0,0 +1,17 @@
+public class SercondLargestInArray {
+    public static void main(String [] args){
+        int arr[]={1,2,3,4,5,66,78};
+        int largest = arr[0];
+        int secondLargest = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>largest){
+                secondLargest = largest;
+                largest=arr[i];
+            }
+            else if(arr[i] > secondLargest && arr[i] != largest){
+                secondLargest=arr[i];
+            }
+        }
+        System.out.println("Second Largest -> " + secondLargest);
+    }
+}

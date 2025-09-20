2681a1de4cfbdd218cc68c265735fe2ee6ba67d7
yash-upadhyay-2003 <yash.upadhyay2022@glbajajgroup.org>
2025-09-20 11:19:01 +0530

    day 7.2: Simplest way to fing sum and Average

--- SumAndAverage.java
+++ SumAndAverage.java
@@ -1,13 +1,14 @@
 public class SumAndAverage{
     public static void main(String[] args){
-        int[] arr={2,2,2,4,6,8};
-        //int average = 0;
+        int[] arr={2,2,2,4,6,9,8};
         int sum =0;
+        double average =0;
         for(int i=0;i<arr.length;i++){
             sum+=arr[i];
             
         }
         System.out.println("Sum ->" + sum);
-        System.out.println("Average-> " + sum/arr.length);
+        average =(double) sum/arr.length;
+        System.out.println("Average-> " + average);
     }
 }

22f2e97d0769035eacd3457ac5d87d0c5304da52
yash-upadhyay-2003 <yash.upadhyay2022@glbajajgroup.org>
2025-09-24 19:51:50 +0530

    Day 12.3:Compressing String by counting repetetions

---
+++ CompressString.java
@@ -0,0 +1,21 @@
+public class CompressString{
+    public static String compress(String str){
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<str.length();i++){
+            Integer count=1;
+            while(i<str.length()-1  && str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(count>1){
+                sb.append(count.toString());
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args){
+        String str = "aaabbbccd";
+        System.out.print("Compressed String: " + compress(str));
+    }
+}

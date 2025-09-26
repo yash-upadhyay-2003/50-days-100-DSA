dc1a2a127e2b0fc91706069580dd8228f2522c8a
yash-upadhyay-2003 <yash.upadhyay2022@glbajajgroup.org>
2025-09-27 00:00:00 +0530

    Day 14.3: ReverseString

---
+++ ReverseWords.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class ReverseWords {
+    public static String reverseWords(String sentence) {
+        // Split sentence into words (by spaces)
+        String[] words = sentence.trim().split("\\s+");
+
+        // Use StringBuilder to reverse order
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i != 0) {
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+        String reversed = reverseWords(sentence);
+        System.out.println("Reversed words: " + reversed);
+    }
+}

f895b123321011f9e08bd4faf427bda71c2bc512
yash-upadhyay-2003 <yash.upadhyay2022@glbajajgroup.org>
2025-09-30 22:28:18 +0530

    day 17

---
+++ RotateKTimes.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class RotateKTimes {
+    public static void rotate(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+    }
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        rotate(arr, k);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        sc.close();
+    }
+}

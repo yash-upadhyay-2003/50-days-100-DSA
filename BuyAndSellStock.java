8a7084ef8cd3d771200b8d77b812f7cbb1e40af9
yash-upadhyay-2003 <yash.upadhyay2022@glbajajgroup.org>
2025-10-02 23:48:08 +0530

    Day 19.3:Buy and sell stock

---
+++ BuyAndSellStock.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class BuyAndSellStock {
+
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] prices = new int[n];
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+        System.out.println(maxProfit(prices));
+        sc.close();
+    }
+}

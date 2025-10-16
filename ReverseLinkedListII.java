129d25420e674fe526a92c1bc6c0c3f909bf342d
yash-upadhyay-2003 <yash.upadhyay2022@glbajajgroup.org>
2025-10-16 13:05:31 +0530

    Day 31:92. Reverse Linked List II

---
+++ ReverseLinkedListII.java
@@ -0,0 +1,66 @@
+// 92. Reverse Linked List II
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class ReverseLinkedListII {
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+
+        ListNode start = prev.next;
+        ListNode then = start.next;
+
+        for (int i = 0; i < right - left; i++) {
+            start.next = then.next;
+            then.next = prev.next;
+            prev.next = then;
+            then = start.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Example: head = [1,2,3,4,5], left = 2, right = 4
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        head = reverseBetween(head, 2, 4);
+
+        System.out.println("Reversed List (positions 2 to 4):");
+        printList(head);
+    }
+}

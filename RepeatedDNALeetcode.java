f58f3b39e9ac94dc0edaf1e52957d6bd030dba58
Yash Upadhyay <yash.upadhyay2022@glbajajgroup.org>
2025-11-02 23:55:09 +0530

    Add method to find repeated DNA sequences
    Implement a solution to find repeated DNA sequences of length 10 in a given string.

---
+++ RepeatedDNALeetcode.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> ans = new HashSet<>();
+        Set<String> seen = new HashSet<>();
+
+        for (int i = 0; i + 10 <= s.length(); ++i) {
+            final String seq = s.substring(i, i + 10);
+            if (seen.contains(seq))
+                ans.add(seq);
+            seen.add(seq);
+        }
+
+        return new ArrayList<>(ans);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        List<String> result = solution.findRepeatedDnaSequences(s);
+        System.out.println("Repeated DNA Sequences: " + result);
+    }
+}
